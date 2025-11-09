fc1f72cb1bc26392245cccf498a8aaea2b9daca5
Kiboom <kiboomy@gmail.com>
2025-11-09 12:10:14 +0900

    Deploy: 2025-11-09 12:10:13

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "766c4320820850d2d9c7805fb088855e",
+const RESOURCES = {"flutter_bootstrap.js": "d8d6a6e2840870268ae6ab345f569c22",
 "version.json": "78bb05588556bde1292494918b562cbe",
 "index.html": "b4fce3c76b50b05b8e94f39b78f4aa2e",
 "/": "b4fce3c76b50b05b8e94f39b78f4aa2e",
-"main.dart.js": "46bd99fa5a89f3e60d9e8eb7c16296a1",
+"main.dart.js": "587d716f1e17a98b62e6f671e5370c66",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "f75131be7a534bf186783cec23c13a00",
 "icons/Icon-192.png": "f75131be7a534bf186783cec23c13a00",
